69f17bfede6ccfea70677eb06c6dd76305429ca8
Alice Pote <alice.writes.wrongs@gmail.com>
2015-07-14 15:53:23 -0400

    Added readlines, writelines, and mygetline to utility.h

--- chapter5/utility.h
+++ chapter5/utility.h
@@ -4,6 +4,9 @@
 
 int getch(void);
 void ungetch(int);
+int readlines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines);
+int mygetline(char *, int);
 
 static char buf[BUFSIZE];
 static int bufp = 0;
@@ -20,3 +23,44 @@ void ungetch(int c)
             else
                         buf[bufp++] = c;
 }
+
+int readlines(char *lineptr[], int maxlines)
+{
+    int len, nlines;
+    char *p, line[MAXLEN];
+
+    nlines = 0;
+    while ((len = mygetline(line, MAXLEN)) > 0)
+        if (nlines >= maxlines || (p = alloc(len)) == NULL)
+            return -1;
+        else {
+            line[len-1] = '\0';
+            strcpy(p, line);
+            lineptr[nlines++] = p;
+        }
+    return nlines;
+}
+
+void writelines(char *lineptr[], int nlines)
+{
+    int i;
+
+    for (i = 0; i < nlines; i++)
+        printf("%s\n", lineptr[i]);
+}
+
+int mygetline(char s[], int lim)
+{
+    int c,i;
+
+    for (i=0; (c=getchar()) != EOF && c!='\n' && i < lim -1; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+
